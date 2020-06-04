e630378792a6bcf081a35eae0f0d4e7a21b577f9
Amelia Meles <ameles@google.com>
2020-06-04 11:42:10 -0700

    initial commit, change year in start script

--- run_bladerfGPS.sh
+++ run_bladerfGPS.sh
@@ -3,8 +3,8 @@
 year=`date -u +%Y`
 day=`date -u +%j`
 
-brdc_file="brdc${day}0.16n"
-brdc_file_year="brdc${year}_${day}0.16n"
+brdc_file="brdc${day}0.${year: -2}n"
+brdc_file_year="brdc${year}_${day}0.${year: -2}n"
 
 if [ ! -e "$brdc_file_year" ]; then
 	wget ftp://cddis.gsfc.nasa.gov/gnss/data/daily/${year}/brdc/${brdc_file}.Z -O ${brdc_file_year}.Z
