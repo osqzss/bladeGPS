650e2beaa1538a25dc82bafa1ae8b07a124cc304
Amelia Meles <ameles@google.com>
2020-06-22 14:13:58 -0700

    changed ftp to wget https call with authentication

--- run_bladerfGPS.sh
+++ run_bladerfGPS.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+export WGETRC=./.wgetrc
 year=`date -u +%Y`
 day=`date -u +%j`
 
@@ -7,8 +8,7 @@ brdc_file="brdc${day}0.${year: -2}n"
 brdc_file_year="brdc${year}_${day}0.${year: -2}n"
 
 if [ ! -e "$brdc_file_year" ]; then
-	wget ftp://cddis.gsfc.nasa.gov/gnss/data/daily/${year}/brdc/${brdc_file}.Z -O ${brdc_file_year}.Z
-	
+	wget --auth-no-challenge "https://cddis.nasa.gov/archive/gnss/data/daily/${year}/brdc/${brdc_file}.Z" -O ${brdc_file_year}.Z
 	if [ ! -e "${brdc_file_year}.Z" ]; then
 		echo "Can't download BRDC file"
 		exit
